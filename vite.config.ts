cdaa2bfe4fea1114585ff6007e0d4ffcdafab13c
Danny Daniels <ydaniels85@gmail.com>
2026-07-29 21:00:34 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,3 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true
-      }
-    }
-  }
-});
+export default defineConfig({ plugins: [react()], server: { port: 5173 } });
